2f96c66c5a9674e3b5244a3834b65c3b65d58614
nov05 <215590+Nov05@users.noreply.github.com>
2024-03-01 06:14:05 -0600

    deepl.__init__, if platform.system()=='Windows' import os, platform if platform.system()=='Windows':     os.add_dll_directory(r"C:/Users/guido/.mujoco/mjpro150/bin")     os.add_dll_directory(r"C:/Users/guido/.mujoco/mujoco-py-1.50.1.68/mujoco_py")     os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'

--- python/deeprl/__init__.py
+++ python/deeprl/__init__.py
@@ -1,7 +1,8 @@
-import os
-os.add_dll_directory(r"C:/Users/guido/.mujoco/mjpro150/bin")
-os.add_dll_directory(r"C:/Users/guido/.mujoco/mujoco-py-1.50.1.68/mujoco_py")
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+import os, platform
+if platform.system()=='Windows':
+    os.add_dll_directory(r"C:/Users/guido/.mujoco/mjpro150/bin")
+    os.add_dll_directory(r"C:/Users/guido/.mujoco/mujoco-py-1.50.1.68/mujoco_py")
+    os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 from .agent import *
 from .component import *
